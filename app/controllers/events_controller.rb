f85fd2da690a3dc2379f5fabf6b85cf5f52cd6e5
shareena fatima <shareenafatima40@gmail.com>
2024-04-08 23:58:48 +0500

    add new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,10 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: %i[index show]
+
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order("created_at desc")
   end
 
   # GET /events/1 or /events/1.json
@@ -12,7 +13,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event =  current_user.events.build
   end
 
   # GET /events/1/edit
@@ -21,7 +22,7 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
@@ -61,6 +62,10 @@ class EventsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
+    end
+
+    def authorize_user!
+      redirect_to root_path, alert: 'You are not authorized to perform this action.' unless current_user == @event.user
     end
 
     # Only allow a list of trusted parameters through.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def event_author(event)
+    user_signed_in? && current_user == event.user
+  end
 end
